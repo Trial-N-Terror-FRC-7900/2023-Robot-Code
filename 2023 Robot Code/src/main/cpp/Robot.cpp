ca4af12f241ea3ae773f1e71d7a072532df9cbdf
TrialNTerror <frc7900@gmail.com>
2023-01-10 20:15:27 -0600

    Added Deadband Code

--- 2023 Robot Code/src/main/cpp/Robot.cpp
+++ 2023 Robot Code/src/main/cpp/Robot.cpp
@@ -6,11 +6,51 @@
 #include <frc/TimedRobot.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/motorcontrol/PWMSparkMax.h>
+#include <photonlib/PhotonUtils.h>
+#include "frc/smartdashboard/SmartDashboard.h"
+#include "ctre/Phoenix.h"
+#include "rev/CANSparkMax.h"
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class.
- * Runs the motors with arcade steering.
- */
+// Returns a 1 for postive numbers and a -1 for negative numbers
+template <typename T> double sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+// Proper Deadband Code to correctly scale the input
+double Deadband(double input, double limit){
+  double output;
+
+  if( fabs(input) < limit){
+    output = 0;
+  }
+  else{
+    output = (1 / (1 - limit)) * (input + (-sgn(input) * limit));
+  }
+
+
+  return output;
+}
+// Proper Deadband Code but with power scaling
+double Deadband(double input, double limit, int power_scale){
+  double output;
+
+  if( fabs(input) < limit){
+    output = 0;
+  }
+  else{
+    output = (1 / (1 - limit)) * (input + (-sgn(input) * limit));
+  }
+  if(power_scale == 2){
+    output = output * output;
+    output = sgn(input) * output;
+  }
+  if(power_scale == 3){
+    output = output * output * output;
+  }
+
+  return output;
+}
+
 class Robot : public frc::TimedRobot {
   frc::PWMSparkMax m_leftMotor{9};
   frc::PWMSparkMax m_rightMotor{1};
@@ -25,9 +65,38 @@ class Robot : public frc::TimedRobot {
     m_rightMotor.SetInverted(true);
   }
 
+
+  // Auto Area
+  void AutonomousInit() override {
+
+  }
+
+  void AutonomousPeriodic() override {
+   
+  }
+
+
+  // Teleop Area
+  void TeleopInit() override {
+
+  }
+
   void TeleopPeriodic() override {
+    double StickX = Deadband(-m_stick.GetX(), 0.05, 2);
+    double StickY = Deadband(-m_stick.GetY(), 0.05, 2);
+
     // Drive with arcade style
-    m_robotDrive.ArcadeDrive(-m_stick.GetY(), -m_stick.GetX());
+    m_robotDrive.ArcadeDrive(StickY, StickX);
+  }
+
+
+  // Test Methods
+  void TestInit() override {
+
+  }
+
+  void TestPeriodic() override {
+
   }
 };
 
